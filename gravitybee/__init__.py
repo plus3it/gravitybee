7e25e9f42579469f08070e20791f69066c17f0ef
Dirk Avery <dirk.avery@gmail.com>
2018-06-06 13:09:11 -0400

    Bump

--- gravitybee/__init__.py
+++ gravitybee/__init__.py
@@ -34,7 +34,7 @@ import json
 from string import Template
 import hashlib
 
-__version__ = "0.1.14"
+__version__ = "0.1.15"
 VERB_MESSAGE_PREFIX = "[GravityBee]"
 EXIT_OKAY = 0
 FILE_DIR = ".gravitybee"
